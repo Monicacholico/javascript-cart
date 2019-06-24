8c1a84aad09764d09f3d944266b3e0ed5a9eced4
Monicacholico <monicachol@hotmail.com>
2019-06-24 08:43:30 -0500

    saving

--- js/app.js
+++ js/app.js
@@ -6,13 +6,28 @@
     const cart = document.getElementById('cart');
 
 
+    cartInfo.addEventListener('click', function(){
+        cart.classList.toggle('show-cart');
+    })
+
+})();
+
+// add items to the cart
 
 
+(function(){
 
+    const cartBtn = document.querySelectorAll('.store-item-icon');
 
+    cartBtn.forEach(function(btn){
+        btn.addEventListener('click', function(event){
+            // console.log(event.target);
+            if(event.target.parentElement.classList('store-item-icon')){
+                console.log(event.target.parentElement);
+            }
 
-
-
+        })
+    });
 
 
 
