becc736a94f8eb51b269567c332a627313bfaac2
Monicacholico <monicachol@hotmail.com>
2019-06-30 23:23:48 -0500

    building item object

--- js/app.js
+++ js/app.js
@@ -25,8 +25,29 @@
 
             if(event.target.parentElement.classList.contains('store-item-icon')){
 
+                let fullPath =event.target.parentElement.previousElementSibling.src;
+                let position = fullPath.indexOf('img') + 3;
+                let partPath = fullPath.slice(position);
+                // console.log(partPath);
 
-                console.log(event.target.parentElement.previousElementSibling.src);
+                const item = {};
+                item.img = `img-cart${partPath}`;
+
+                let name = event.target.parentElement.parentElement.nextElementSibling.
+                children[0].children[0].textContent;
+                item.name = name;
+
+                let price = event.target.parentElement.parentElement.nextElementSibling.
+                    children[0].children[1].textContent;
+
+                let finalPrice = price.slice(1).trim();
+                item.price = finalPrice;
+
+                console.log(finalPrice);
+
+                // console.log(name);
+
+                console.log(item);
             }
 
 
