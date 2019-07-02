50a424490f6059aa71e5cb74abf6be3057cff020
Monicacholico <monicachol@hotmail.com>
2019-07-02 14:43:17 -0500

    finished

--- js/app.js
+++ js/app.js
@@ -89,7 +89,21 @@
             total.push(parseFloat(item.textContent));
         });
 
-        console.log(total);
+        // console.log(total);
+
+        const totalMoney = total.reduce(function(total, item){
+            total += item;
+            return total;
+        }, 0);
+
+        const finalMoney = totalMoney.toFixed(2);
+        console.log(finalMoney);
+
+        document.getElementById("cart-total").textContent = finalMoney;
+        document.querySelector(".item-total").textContent = finalMoney;
+        document.getElementById("item-count").textContent = total.length;
+
+
     }
 
 
